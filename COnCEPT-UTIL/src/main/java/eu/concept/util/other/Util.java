f3a23df1ac58652cc8aa85674cd3e007262ac99d
Christos Paraskeva <cparaskeva@ubitech.eu>
2015-06-16 10:38:04 +0300

    Modifications to Thymleaf templates

--- COnCEPT-UTIL/src/main/java/eu/concept/util/other/Util.java
+++ COnCEPT-UTIL/src/main/java/eu/concept/util/other/Util.java
@@ -3,6 +3,8 @@ package eu.concept.util.other;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Random;
+import java.util.UUID;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -12,16 +14,14 @@ import java.util.logging.Logger;
  */
 public class Util {
 
-    
     /**
      * Encodes a String based on a specific algorithm
-     * 
+     *
      * @param content The String to encrypt
      * @param algorithm The algorithm to be used
      * @return The encoded String
      */
     public static String createAlgorithm(String content, String algorithm) {
-
         String encryptedContent = "";
         try {
             //Producing the SHA hash for the input
@@ -37,10 +37,25 @@ public class Util {
         return encryptedContent;
     }
 
-    
-    
-    public static void main(String[] args){
-        System.out.println( Util.createAlgorithm("!admin!", "SHA"));
+    public static String getRandomHexString(int numchars) {
+        Random r = new Random();
+        StringBuilder sb = new StringBuilder();
+        while (sb.length() < numchars) {
+            sb.append(Integer.toHexString(r.nextInt()));
+        }
+
+        return sb.toString().substring(0, numchars);
     }
-    
+
+    public static void main(String[] args) {
+        String sha1 = Util.createAlgorithm("ttest", "SHA");
+        System.out.println("SHA1: " + sha1);
+        String salt = "689dc1036b90261c8f69c1666fc60e3f";
+        String sha1_salt = Util.createAlgorithm(salt + sha1, "SHA");
+        System.out.println("SHA1(slat+SHA1(palin_text)) =  " + sha1_salt);
+
+        System.out.println(getRandomHexString(32));
+
+    }
+
 }
