3e4a3d7f061d84b4738c4c0ffd9a89cded9de7d8
George Tsiolis <tsiolis.g@gmail.com>
2015-07-11 18:15:10 +0300

    Added categories annotation multiple selection

--- COnCEPT-PLATFORM-APP/src/main/webapp/resources/js/dashboard.js
+++ COnCEPT-PLATFORM-APP/src/main/webapp/resources/js/dashboard.js
@@ -102,67 +102,92 @@ var data = [
     {
         label: 'Product Category', id: 1,
         children: [
-            {label: 'Kitchenware', id: 2},
-            {label: 'Exhibition', id: 3},
-            {label: 'Lighting', id: 4},
-            {label: 'Furniture', id: 5}
+            {label: 'Kitchenware', id: 11},
+            {label: 'Exhibition', id: 12},
+            {label: 'Lighting', id: 13},
+            {label: 'Furniture', id: 14}
         ]
     },
     {
-        label: 'Product Domain', id: 10,
+        label: 'Product Domain', id: 2,
         children: [
-            {label: 'Medical', id: 11},
-            {label: 'Cosumer', id: 12},
-            {label: 'Sport', id: 13},
-            {label: 'Market Analysis', id: 14},
-            {label: 'Technology', id: 15},
-            {label: 'Usability', id: 16}
+            {label: 'Medical', id: 21},
+            {label: 'Cosumer', id: 22},
+            {label: 'Sport', id: 23},
+            {label: 'Market Analysis', id: 24},
+            {label: 'Technology', id: 25},
+            {label: 'Usability', id: 26}
         ]
     },
     {
-        label: 'Product Language', id: 10,
+        label: 'Product Language', id: 3,
         children: [
             {
-                label: 'Style', id: 10,
+                label: 'Style', id: 31,
                 children: [
                     {
-                        label: 'Period Style', id: 10,
+                        label: 'Period Style', id: 311,
                         children: [
-                            {label: 'Classic', id: 11},
-                            {label: 'Chic', id: 12},
-                            {label: 'Modern', id: 13},
-                            {label: 'Artdeco', id: 14}
+                            {label: 'Classic', id: 3112},
+                            {label: 'Chic', id: 3113},
+                            {label: 'Modern', id: 3113},
+                            {label: 'Artdeco', id: 3115}
                         ]
                     },
                     {
-                        label: 'Partial Style', id: 10,
+                        label: 'Partial Style', id: 312,
                         children: [
-                            {label: 'National', id: 11},
-                            {label: 'Corporate', id: 12},
-                            {label: 'Target Style', id: 13}
+                            {label: 'National', id: 3121},
+                            {label: 'Corporate', id: 3122},
+                            {label: 'Target Style', id: 3123}
                         ]
                     }
                 ]
             },
             {
-                label: 'Material', id: 10,
+                label: 'Material', id: 32,
                 children: [
-                    {label: 'Steel', id: 11},
-                    {label: 'Stone', id: 12}
+                    {label: 'Steel', id: 321},
+                    {label: 'Stone', id: 322}
                 ]
             },
             {
-                label: 'Associations and Feelings', id: 10,
+                label: 'Associations and Feelings', id: 33,
                 children: [
-                    {label: 'Cold', id: 11},
-                    {label: 'Warm', id: 12},
-                    {label: 'Aggressive', id: 12}
+                    {label: 'Cold', id: 331},
+                    {label: 'Warm', id: 332},
+                    {label: 'Aggressive', id: 333}
                 ]
             }
         ]
     }
 ];
 
-$('#tree').tree({
-    data: data
+
+$(function() {
+    var $tree = $('#tree');
+    $tree.tree({
+        data: data,
+        autoOpen: true,
+    });
+    $tree.bind(
+        'tree.click',
+        function(e) {
+            // Disable single selection
+            e.preventDefault();
+
+            var selected_node = e.node;
+
+            if (selected_node.id == undefined) {
+                console.log('The multiple selection functions require that nodes have an id');
+            }
+
+            if ($tree.tree('isNodeSelected', selected_node)) {
+                $tree.tree('removeFromSelection', selected_node);
+            }
+            else {
+                $tree.tree('addToSelection', selected_node);
+            }
+        }
+    );
 });
