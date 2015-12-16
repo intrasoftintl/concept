59beaaf6b6fc2cf48bcd54b5e3f063ebcd5fd1ed
Christos Paraskeva <cparaskeva@ubitech.eu>
2015-12-16 13:02:51 +0200

    Integrated se ui

--- concept-app/src/main/java/eu/concept/controller/component/SearchController.java
+++ concept-app/src/main/java/eu/concept/controller/component/SearchController.java
@@ -1,34 +1,17 @@
 package eu.concept.controller.component;
 
-import eu.concept.controller.WebController;
 import static eu.concept.controller.WebController.getCurrentUser;
-import eu.concept.repository.concept.domain.BriefAnalysis;
-import eu.concept.repository.concept.domain.Sketch;
 import eu.concept.repository.concept.service.NotificationService;
 import eu.concept.repository.concept.service.SketchService;
 import eu.concept.repository.openproject.domain.ProjectOp;
 import eu.concept.repository.openproject.service.ProjectServiceOp;
-import eu.concept.util.other.NotificationTool;
-import eu.concept.util.other.NotificationTool.NOTIFICATION_OPERATION;
-
-import java.io.IOException;
 import java.util.List;
-import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import javax.xml.bind.DatatypeConverter;
 
 /**
  *
@@ -49,11 +32,13 @@ public class SearchController {
     /*
      *  GET Methods 
      */
-
-    
-    @RequestMapping(value = "/se_app", method = RequestMethod.GET)
-    public String search(Model model) {
-            model.addAttribute("currentUser", getCurrentUser());
+    @RequestMapping(value = "/se_app", method = RequestMethod.POST)
+    public String search(@RequestParam(value = "projectID", defaultValue = "0", required = false) int projectID, Model model) {
+        System.out.println("ProjectId is: " + projectID);
+        List<ProjectOp> projects = projectServiceOp.findProjectsByUserId(getCurrentUser().getId());
+        model.addAttribute("projects", projects);
+        model.addAttribute("currentUser", getCurrentUser());
+        model.addAttribute("search_query_url","query_url");
         return "se_app";
     }
 }
