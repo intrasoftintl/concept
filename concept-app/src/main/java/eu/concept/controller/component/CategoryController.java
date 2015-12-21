e80046a27317fda98f10bcd19be4605ff06d41b6
Christos Paraskeva <cparaskeva@ubitech.eu>
2015-12-21 12:31:13 +0200

    Category Controller

---
+++ concept-app/src/main/java/eu/concept/controller/component/CategoryController.java
@@ -0,0 +1,40 @@
+package eu.concept.controller.component;
+
+import static eu.concept.controller.WebController.getCurrentUser;
+import eu.concept.repository.openproject.domain.ProjectOp;
+import eu.concept.repository.openproject.service.ProjectServiceOp;
+import java.util.List;
+import java.util.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author Christos Paraskeva <ch.paraskeva at gmail dot com>
+ */
+@Controller
+public class CategoryController {
+
+    private static final Logger logger = Logger.getLogger(CategoryController.class.getName());
+
+    @Autowired
+    ProjectServiceOp projectServiceOp;
+
+    @RequestMapping(value = "/category", method = RequestMethod.GET)
+    public String category(Model model) {
+        if (!model.containsAttribute("projectID")) {
+            model.addAttribute("projectID", "0");
+        }
+        List<ProjectOp> projects = projectServiceOp.findProjectsByUserId(getCurrentUser().getId());
+        model.addAttribute("projects", projects);
+        model.addAttribute("currentUser", getCurrentUser());
+        return "category";
+    }
+    
+    
+    
+
+}
