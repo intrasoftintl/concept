2d729dae9f7a6c08cfba618a97f4b58781c40bb7
Christos Paraskeva <cparaskeva@ubitech.eu>
2015-12-21 18:35:20 +0200

    Fixes

--- concept-app/src/main/java/eu/concept/controller/component/CategoryController.java
+++ concept-app/src/main/java/eu/concept/controller/component/CategoryController.java
@@ -1,6 +1,9 @@
 package eu.concept.controller.component;
 
+import static eu.concept.controller.WebController.getCurrentUser;
+import eu.concept.repository.openproject.domain.ProjectOp;
 import eu.concept.repository.openproject.service.ProjectServiceOp;
+import java.util.List;
 import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -21,17 +24,31 @@ public class CategoryController {
     @Autowired
     ProjectServiceOp projectServiceOp;
 
+    /*
+     *  GET Methods 
+     */
+    
+    
+    /*
+     *  POST Methods 
+     */
+    @RequestMapping(value = "/category_app", method = RequestMethod.POST)
+    public String hierarchy_app(Model model, @RequestParam(value = "projectID", defaultValue = "0", required = true) String projectID) {
+        List<ProjectOp> projects = projectServiceOp.findProjectsByUserId(getCurrentUser().getId());
+        model.addAttribute("projects", projects);
+        model.addAttribute("currentUser", getCurrentUser());
+        model.addAttribute("projectID", projectID);
+        return "category_app";
+    }
+
     @RequestMapping(value = "/category/add", method = RequestMethod.POST)
     public String addCategory(Model model, @RequestParam(value = "projectID", defaultValue = "", required = true) String projectID, @RequestParam(value = "categoryName", defaultValue = "", required = true) String categoryName, @RequestParam(value = "parentCategoryID", defaultValue = "", required = false) String parentCategoryID) {
         if (!model.containsAttribute("projectID")) {
             model.addAttribute("projectID", "0");
         }
-
-        System.out.println("CategoryName: " + categoryName);
-        System.out.println("parentCategoryID: " + parentCategoryID);
-        System.out.println("projectID: " + projectID);
-        
-        return "hierarchy_app";
+        List<ProjectOp> projects = projectServiceOp.findProjectsByUserId(getCurrentUser().getId());
+        model.addAttribute("projects", projects);
+        return "category_app";
     }
 
 }
