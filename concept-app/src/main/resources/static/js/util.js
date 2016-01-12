4ff864e6c6d225fecbfd8226b24a0721aff59c86
Christos Paraskeva <cparaskeva@ubitech.eu>
2016-01-12 12:53:43 +0200

    Fixes

--- concept-app/src/main/resources/static/js/util.js
+++ concept-app/src/main/resources/static/js/util.js
@@ -239,22 +239,24 @@ function projectSelectedAction(projectID) {
 
             $("#ba-add").attr("href", "/ba_app?projectID=" + projectID);
             $("#ba-add").show();
-            $("#ba-all").load("/briefanalysis_all/" + projectID + "?limit=200", function(){
-                
-                var elements = $("[id^=copy-button-]");
-                for(var index=0; index < elements.length; index++){
-                    console.log(elements[index].id);  
-                    console.log(elements.length);
-                 
-                    // Get link function
-                    var client = new ZeroClipboard(document.getElementById(elements[index].id));
-                    client.on("ready", function (readyEvent) {
-                        client.on("aftercopy", function (event) {
-                            alert("Copied text to clipboard: " + event.data["text/plain"]);
-                        });
-                    });
-                }
-                
+            var client = [];
+            $("#ba-all").load("/briefanalysis_all/" + projectID + "?limit=200", function () {
+
+//                var elements = $("[id^=copy-button-]");
+//                for (var index = 0; index < elements.length; index++) {
+//                    // Get link function
+//                    client[index] = new ZeroClipboard(elements[index]);
+//                    var tmp = client[index];
+//
+//                      client[index].on("ready", function (readyEvent) {
+//                          tmp.on("aftercopy", function (event) {
+//                            alert("Copied text to clipboard: " + event.data["text/plain"]);
+//                        });
+//                    });
+//
+//                    console.log('Length: ' + client.length + " index: " + index);
+//                }
+
             });
 
             $("#ba-placeholder").hide();
