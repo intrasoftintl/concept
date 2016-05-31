8cbd1c49055b0f2de394f7b677b2531f41f66a21
Chris <ch.paraskeva@gmail.com>
2016-05-31 12:43:04 +0300

    Fixes

--- concept-repository/src/main/java/eu/concept/repository/concept/dao/TagsRepository.java
+++ concept-repository/src/main/java/eu/concept/repository/concept/dao/TagsRepository.java
@@ -8,11 +8,13 @@ package eu.concept.repository.concept.dao;
 
 import eu.concept.repository.concept.domain.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  *
  * @author Christos Paraskeva <ch.paraskeva at gmail dot com>
  */
+@Repository
 public interface TagsRepository extends JpaRepository<Tag,Integer>{
 
 }
